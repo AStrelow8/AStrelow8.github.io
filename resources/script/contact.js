ca472b5c89b76531b997c5f4013b16bbb83cd56f
AStrelow8 <72233953+AStrelow8@users.noreply.github.com>
2021-03-15 17:14:44 -0700

    Update CSS grid, contact HTML, Contact JS

---
+++ resources/script/contact.js
@@ -0,0 +1,6 @@
+// Make text on contact page green
+document.getElementById('submit').addEventListener('click', function() {
+    document.getElementById('contact-form').style.color = 'green';
+    alert('Thank you for contact request!');
+})
+
